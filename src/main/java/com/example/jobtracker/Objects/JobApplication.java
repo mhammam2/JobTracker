7c692f37fe4f24e4ea44d46200a4fa9be421e2d5
mhammam2 <mhammam1@hotmail.com>
2025-02-19 19:23:45 -0600

    Initial Commit

---
+++ src/main/java/com/example/jobtracker/Objects/JobApplication.java
@@ -0,0 +1,10 @@
+package com.example.jobtracker.Objects;
+
+public class JobApplication
+{
+    private String companyName;
+    private String jobTitle;
+    private String status;
+    private String applicationDate;
+    private String notes;
+}
